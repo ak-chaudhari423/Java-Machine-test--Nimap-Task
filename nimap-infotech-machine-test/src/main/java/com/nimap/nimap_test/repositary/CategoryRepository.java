386449b10a20bdb638984731e95db90ec61d1871
Aakash Chaudhari <168959305+ak-chaudhari423@users.noreply.github.com>
2024-09-30 01:40:30 +0530

    Delete nimap-infotech-machine-test directory

--- nimap-infotech-machine-test/src/main/java/com/nimap/nimap_test/repositary/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nimap.nimap_test.repositary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nimap.nimap_test.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
